01644a1219d55b2f3ed0a1f8e5dcf10a2a7cd223
rixo <rixo@rixo.fr>
2019-09-27 16:16:53 +0200

    go go go

---
+++ README.md
@@ -0,0 +1,70 @@
+# rollup-plugin-svelte-hmr
+
+Svelte HMR with [Nollup](https://github.com/PepsRyuu/nollup).
+
+## Status
+
+This is an experimental implementation of Svelte HMR. I hope this will be useful when official HMR is added to Svelte, but no such plans have been made yet.
+
+This implementation works by abusing Svelte's private API, so stability across versions is all but guaranteed -- although I'm committed to keep it working for the times being.
+
+Also it suffers some limitations that I don't know how to bypass until HMR hooks are implemented in Svelte itself. Most notably, indexed each blocks `{#each item as item, index}` will be messed by HMR updates.
+
+There are probably other flaws. Please report the bugs you encounter so that I can try to fix them for now but, more importantly, document them and build test cases for when official implementation comes.
+
+## Installation
+
+~~~
+npm install --save-dev nollup rollup-plugin-svelte-hmr
+~~~
+
+## Usage
+
+Add it after the Svelte plugin to your `rollup.config.js`:
+
+~~~js
+// ...
+const svelteHmr = require('rollup-plugin-svelte-hmr')
+
+const development = process.env.NODE_ENV !== ''
+
+module.exports = {
+  // ...
+  plugins: [
+    svelte({
+      // ...
+    }),
+    svelteHmr({
+      hot: development,
+    }),
+    // ...
+  ],
+  // ...
+}
+~~~
+
+When the `hot` option is set to `false`, the plugin will do nothing. This is useful for production builds. That's the only option for now.
+
+Then run it:
+
+~~~
+npx nollup -c --verbose
+~~~
+
+An go edit files!
+
+## Known limitations
+
+Indexed each blocks are not supported by HMR. If the items are reordered, then a HRM update of the involved components will mess the order of everything.
+
+~~~
+<!-- NOT SUPPORTED -->
+{#each items item, index}
+  ...
+{/each}
+
+<!-- should work -->
+{#each items item}
+  ...
+{/each}
+~~~
---
+++ index.js
@@ -0,0 +1,122 @@
+const path = require('path')
+const { createFilter } = require('rollup-pluginutils')
+
+const hotApi = path.resolve(`${__dirname}/runtime/hot-api.js`)
+
+const posixify = file => file.replace(/[/\\]/g, '/')
+
+const quote = JSON.stringify
+
+const globalName = '__ROLLUP_PLUGIN_SVELTE_HMR'
+
+const hotApiAlias = 'rollup-plugin-svelte-hmr/_/hot-api'
+
+const aliases = {
+  [hotApiAlias]: hotApi,
+}
+
+const svelteHmr = ({ hot = true, hotOptions = {} } = {}) => {
+  const filter = createFilter('**/*.svelte', [])
+
+  const options = JSON.stringify(hotOptions)
+
+  const compileData = 'undefined' // TODO
+
+  function transform(code, id) {
+    if (!hot) return
+    if (!filter(id)) return
+
+    this.addWatchFile(hotApi)
+
+    const replacement = `
+      import * as ${globalName} from '${hotApiAlias}'
+      if (module.hot) {
+        const { applyHMR } = ${globalName}
+        $2 = applyHMR(
+          ${options},
+          ${quote(id)},
+          module,
+          $2,
+          undefined,
+          ${compileData}
+        );
+      }
+      export default $2;
+    `
+    const transformed = code.replace(/(export default ([^;]*));/, replacement)
+
+    return transformed
+  }
+
+  let fs
+  const _setFs = _fs => {
+    fs = _fs
+  }
+
+  function load(id) {
+    if (!fs) return
+    // const buffer = fs.readFileSync(id, 'utf8');
+    // const code = buffer.toString('utf8')
+    // console.log(id, code)
+    // return code
+    return new Promise((resolve, reject) => {
+      fs.readFile(id, 'utf8', (err, contents) => {
+        if (err) reject(err)
+        else resolve(contents)
+      })
+    })
+  }
+
+  const listeners = {
+    generateBundle: [],
+    renderError: [],
+  }
+
+  const addListener = type => listener => {
+    listeners[type].push(listener)
+  }
+
+  const fire = type => (...args) => {
+    listeners[type].forEach(listener => listener(...args))
+  }
+
+  // function generateBundle(options, bundle, isWrite) {}
+  // function renderError(error) {}
+  const generateBundle = fire('generateBundle')
+  const renderError = fire('renderError')
+
+  const _onBundleGenerated = addListener('generateBundle')
+
+  const _onRenderError = addListener('renderError')
+
+  return {
+    name: 'svelte-hmr',
+    resolveId(target, from) {
+      const alias = aliases[target]
+      if (alias) {
+        return alias
+      }
+      if (fs) {
+        const name = path.join(path.dirname(from), target)
+        const extensions = ['.js', '.svelte']
+        for (const ext of extensions) {
+          const filename = name + ext
+          if (fs.existsSync(filename) && fs.lstatSync(filename).isFile()) {
+            return filename
+          }
+        }
+      }
+    },
+    transform,
+
+    load,
+    _setFs,
+
+    generateBundle,
+    renderError,
+    _onBundleGenerated,
+    _onRenderError,
+  }
+}
+
+module.exports = svelteHmr
