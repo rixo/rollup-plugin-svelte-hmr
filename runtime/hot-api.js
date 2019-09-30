62298de2eb2971687851da7ec47bc189f946e49d
rixo <rixo@rixo.fr>
2019-09-30 11:29:23 +0200

    upgrade to last svelte-hmr with error management

--- runtime/hot-api.js
+++ runtime/hot-api.js
@@ -1,26 +1,11 @@
-import { doApplyHMR } from 'svelte-hmr/runtime'
+import { doApplyHmr } from 'svelte-hmr/runtime'
 
 const declinedModules = (window.__ROLLUP_PLUGIN_SVELTE_HMR_DECLINED_MODULES =
   window.__ROLLUP_PLUGIN_SVELTE_HMR_DECLINED_MODULES || {})
 
-const reload = () => {
-  if (
-    typeof window !== 'undefined' &&
-    window.location &&
-    window.location.reload
-  ) {
-    window.location.reload()
-  }
-}
+export function applyHmr(args) {
+  const { m, id, hotOptions } = args
 
-export function applyHMR(
-  targetModule,
-  id,
-  hotOptions,
-  Component,
-  ProxyAdapter,
-  compileData
-) {
   if (declinedModules[id]) {
     declinedModules[id] = false
     if (!hotOptions.noReload) {
@@ -28,27 +13,13 @@ export function applyHMR(
     }
   }
 
-  try {
-    const { proxy, error } = doApplyHMR(
-      hotOptions,
-      id,
-      Component,
-      ProxyAdapter,
-      compileData
-    )
-
-    if (error && !hotOptions.optimistic) {
-      declinedModules[id] = true
-    } else {
-      targetModule.hot.accept(() => require(targetModule.id))
-    }
-
-    return proxy
-  } catch (err) {
+  const decline = () => {
     declinedModules[id] = true
-    // since we won't return the proxy and the app will expect a svelte
-    // component, it's gonna crash... so it's best to report the real cause
-    throw err
-    // throw new Error(`Failed to create HMR proxy for Svelte component ${id}`)
   }
+
+  const accept = () => {
+    m.hot.accept(() => require(m.id))
+  }
+
+  return doApplyHmr({ ...args, accept, decline })
 }
